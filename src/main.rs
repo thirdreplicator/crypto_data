aa0bad1b5f7521c0013ed412ce8ed3de129eff53
David Beckwith <thirdreplicator@gmail.com>
2022-04-19 20:25:54 +0800

    Make a request (using reqwest) to Binance Kline HTTP API.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+//use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Kline(
+  i64,
+  String,
+  String,
+  String,
+  String,
+  String,
+  i64,
+  String,
+  i64,
+  String,
+  String,
+  String,
+);
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=10")
+        .await?
+        .json::<Vec<Kline>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
+
