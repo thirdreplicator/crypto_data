d30308bc7bece98898a410a758076b924847adb4
David Beckwith <thirdreplicator@gmail.com>
2022-04-24 10:47:19 +0800

    Moved http request to candle.rs

--- src/candle.rs
+++ src/candle.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize};
+use reqwest::Error;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Candle(
@@ -15,3 +16,11 @@ pub struct Candle(
   pub String,
   pub String,
 );
+
+pub async fn request() -> Result<Vec<Candle>, Error> {
+    let resp = reqwest::get("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=10")
+        .await?
+        .json::<Vec<Candle>>()
+        .await?;
+    Ok(resp)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ use crypto_data::candle;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=10")
-        .await?
-        .json::<Vec<candle::Candle>>()
-        .await?;
-    println!("{:#?}", resp);
+    match candle::request().await {
+        Ok(resp) => println!("{:#?}", resp),
+        _ => println!("Error")
+    }
     Ok(())
 }
 
